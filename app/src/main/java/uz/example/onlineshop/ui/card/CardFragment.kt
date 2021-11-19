33d2bc56398eff79536f831b70b0a79e7b418297
BrighterDeeds <brightDreamPath@gmail.com>
2021-11-19 14:38:17 +0500

    status fragments were done, card fragment also done

---
+++ app/src/main/java/uz/example/onlineshop/ui/card/CardFragment.kt
@@ -0,0 +1,7 @@
+package uz.example.onlineshop.ui.card
+
+import androidx.fragment.app.Fragment
+import uz.example.onlineshop.R
+
+class CardFragment : Fragment(R.layout.fragment_card) {
+}
---
+++ app/src/main/java/uz/example/onlineshop/ui/status/FailureFragment.kt
@@ -0,0 +1,7 @@
+package uz.example.onlineshop.ui.status
+
+import androidx.fragment.app.Fragment
+import uz.example.onlineshop.R
+
+class FailureFragment:Fragment(R.layout.fragment_failure) {
+}
