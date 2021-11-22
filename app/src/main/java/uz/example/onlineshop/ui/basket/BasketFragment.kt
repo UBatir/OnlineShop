0fda68023858d3838d1f674c2108bf0777097afe
BrighterDeeds <brightDreamPath@gmail.com>
2021-11-22 15:49:02 +0500

    bottom dialog was done

--- app/src/main/java/uz/example/onlineshop/ui/basket/BasketFragment.kt
+++ app/src/main/java/uz/example/onlineshop/ui/basket/BasketFragment.kt
@@ -1,18 +1,22 @@
 package uz.example.onlineshop.ui.basket
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.SimpleExpandableListAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.gson.GsonBuilder
 import org.koin.android.ext.android.inject
 import uz.example.onlineshop.R
 import uz.example.onlineshop.core.extentions.onClick
 import uz.example.onlineshop.data.remote.Product
+import uz.example.onlineshop.databinding.DialogBottomSheetBinding
 import uz.example.onlineshop.databinding.FragmentBasketBinding
-import uz.example.onlineshop.ui.main.MainFragmentDirections
+
 
 class BasketFragment: Fragment(R.layout.fragment_basket) {
 
@@ -20,6 +24,11 @@ class BasketFragment: Fragment(R.layout.fragment_basket) {
     private lateinit var navController: NavController
     private val adapter: BasketAdapter by inject()
     private var sum = 0L
+
+    private val mGroupsArray = arrayListOf( "Способ оплаты")
+
+    private val mWinterMonthsArray = arrayListOf ( "Наличные", "Карта")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding= FragmentBasketBinding.bind(view)
@@ -53,6 +62,46 @@ class BasketFragment: Fragment(R.layout.fragment_basket) {
             val gsonPretty = GsonBuilder().setPrettyPrinting().create()
             val gsonString = gsonPretty.toJson(it)
             findNavController().navigate(BasketFragmentDirections.actionBasketFragmentToDetailFragment(gsonString))
+        }
+        binding.btnOrder.onClick{
+            val dialog = BottomSheetDialog(requireContext())
+            val bottomView = LayoutInflater.from(context).inflate(R.layout.dialog_bottom_sheet, null)
+            val bindingD=DialogBottomSheetBinding.bind(bottomView)
+
+
+            var map: MutableMap<String?, String?>
+            val groupDataList: ArrayList<Map<String?, String?>> = ArrayList()
+            for (group in mGroupsArray) {
+                map = HashMap()
+                map["groupName"] = group
+                groupDataList.add(map)
+            }
+            val groupFrom = arrayOf("groupName")
+            val groupTo = intArrayOf(android.R.id.text1)
+            val сhildDataList: ArrayList<ArrayList<Map<String?, String?>?>> = ArrayList()
+            val сhildDataItemList: ArrayList<Map<String?, String?>?> = ArrayList()
+            for (month in mWinterMonthsArray) {
+                map = HashMap()
+                map["monthName"] = month // название месяца
+                сhildDataItemList.add(map)
+            }
+            сhildDataList.add(сhildDataItemList)
+            val childFrom = arrayOf("monthName")
+            val childTo = intArrayOf(R.id.rdButton)
+
+            val adapter = SimpleExpandableListAdapter(
+                requireContext(), groupDataList,
+                android.R.layout.simple_expandable_list_item_1, groupFrom,
+                groupTo, сhildDataList, R.layout.radiobutton_list_item_1,
+                childFrom, childTo
+            )
+            bindingD.btnSelectCart.setAdapter(adapter)
+
+            bindingD.btnSelectCart.setOnItemClickListener { parent, view, position, id ->
+
+            }
+            dialog.setContentView(bottomView)
+            dialog.show()
         }
     }
 }
